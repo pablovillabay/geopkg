32696961b46c549c0fef4c44bb8dec206fcaaec2
pablovillabay <pablovillabay@gmail.com>
2021-12-09 12:02:05 +0100

    clean up

--- geopkg/io/file_length.py
+++
@@ -1,8 +0,0 @@
-def file_length(path):
-
-    """Simply count the lines in a text file."""
-
-    with open(path) as f:
-        for i, l in enumerate(f):  # read through the lines in the file (i counts and l stores each line)
-            pass
-    return i + 1
